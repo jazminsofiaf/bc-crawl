15d3a72eca9fc92791a839714b5428e7d90e1781
JazminF <Jazmina@JazsMacBookPro.local>
2020-04-02 03:19:29 -0300

    static mut vars

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ use std::io::Write;
 use std::sync::mpsc;
 use std::thread;
 
+static mut BEAT : bool = false;
+static mut PEER_OUTPUT_FILE_NAME: String = String::new();
 
-fn parse_args(beat: &mut bool, file_name: &mut String) -> String {
+
+fn parse_args() -> String {
     let matches = App::new("BC crawl")
         .version("1.0.0")
         .author("Jazmin Ferreiro  <jazminsofiaf@gmail.com>")
@@ -39,51 +42,58 @@ fn parse_args(beat: &mut bool, file_name: &mut String) -> String {
 
     let arg_beat = matches.is_present("beat");
     if arg_beat{
-        *beat = true;
+        unsafe{
+            BEAT = true;
+        };
         return String::from(arg_address);
     }
 
     let arg_file = matches.value_of("file");
     match arg_file {
         None => panic!("Error parsing file name (not beat flag)"),
-        Some(f) => {
-            file_name.push_str(f);
-            File::create(file_name).expect("failed create file");
+        Some(f) =>  unsafe {
+            PEER_OUTPUT_FILE_NAME.push_str(f);
+            File::create(f).expect("failed create file");
         }
     }
 
     return String::from(arg_address);
 }
-fn store_event(beat: bool, file_name: & String, msg :&String){
+
+fn store_event(beat: bool, msg :&String){
     if beat {
         print!("beat\n");
         return;
     }
-    let mut peer_log_file:File =  OpenOptions::new().append(true).open(file_name).expect("filed to open file");
-    peer_log_file.write_all( msg.as_bytes()).expect("failed to write in file");
 
+    unsafe{
+        let file_name = String::from(&PEER_OUTPUT_FILE_NAME);
+        let mut peer_log_file:File =  OpenOptions::new().append(true).open(file_name).expect("filed to open file");
+        peer_log_file.write_all( msg.as_bytes()).expect("failed to write in file");
+    }
 }
+
+
 fn main() {
 
     crab::foo();
 
+
     let (address_channel_tx,address_channel_rx) = mpsc::channel();
-    let (file_name_tx,file_name_rx) = mpsc::channel();
-    let mut beat: bool = false;
+
 
     thread::spawn(move || {
-        let mut file_name: String = String::new();
-        let address = parse_args(&mut beat, &mut file_name);
+        let address = parse_args();
         address_channel_tx.send(address).unwrap();
-        file_name_tx.send(file_name).unwrap();
     });
 
 
-    let file_name = file_name_rx.recv().unwrap();
-
     let msg:String = format!("name: {}\n", address_channel_rx.recv().unwrap());
-    store_event(beat, & file_name, & msg);
-    store_event(beat, & file_name, & msg );
+    unsafe {
+        store_event(BEAT, & msg);
+        store_event(BEAT, & msg );
+    }
+
 
 }
 
