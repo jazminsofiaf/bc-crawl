048908a1737ebb3fbd98ea26837a172a7f7c0744
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-04-05 11:29:47 -0300

    test share adresses to test

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@ use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Duration;
 
-fn m(counter : Arc<Mutex<i64>>){
-    thread::sleep(Duration::from_millis(1));
-    println!("restando: {}", *counter.lock().unwrap());
-    let mut num = counter.lock().unwrap();
-    *num += -1;
-}
-
-fn c(counter : Arc<Mutex<i64>>){
+fn handle_one_peer(counter : Arc<Mutex<i64>>){
     for _ in 0..10 {
         thread::sleep(Duration::from_millis(1));
-        println!("checkeando: {}", *counter.lock().unwrap());
+        println!("handle one peer: {}", *counter.lock().unwrap());
+        let mut num = counter.lock().unwrap();
+        *num += -1;
+    }
+
+
+}
+
+fn check_pool_size(counter : Arc<Mutex<i64>>){
+    loop {
+        thread::sleep(Duration::from_millis(1));
+        println!("check pool size: {}", *counter.lock().unwrap());
 
         if *counter.lock().unwrap() == 0 {
             println!("chau");
@@ -31,17 +35,17 @@ fn main() {
 
     let c2 = Arc::clone(&counter);
     let handle2 = thread::spawn(move || {
-        c(c2)
+        check_pool_size(c2)
     });
     handles.push(handle2);
 
-    for _ in 0..10 {
-        let c1 = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            m(c1)
-        });
-        handles.push(handle);
-    }
+
+    let c1 = Arc::clone(&counter);
+    let handle = thread::spawn(move || {
+        handle_one_peer(c1)
+    });
+    handles.push(handle);
+
 
     for _ in 0..10 {
         let mut num = counter.lock().unwrap();
