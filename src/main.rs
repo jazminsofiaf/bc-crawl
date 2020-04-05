897ca5149ab53f886f1b05d16d4880951475f328
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-04-05 11:59:43 -0300

    infinite loop

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::thread;
 use std::time::Duration;
 
 fn handle_one_peer(counter : Arc<Mutex<i64>>){
-    for _ in 0..10 {
+    loop {
         thread::sleep(Duration::from_millis(1));
         println!("handle one peer: {}", *counter.lock().unwrap());
         let mut num = counter.lock().unwrap();
@@ -19,7 +19,7 @@ fn check_pool_size(counter : Arc<Mutex<i64>>){
         thread::sleep(Duration::from_millis(1));
         println!("check pool size: {}", *counter.lock().unwrap());
 
-        if *counter.lock().unwrap() == 0 {
+        if *counter.lock().unwrap() < 0 {
             println!("chau");
             process::exit(0);
         }
@@ -31,31 +31,24 @@ fn check_pool_size(counter : Arc<Mutex<i64>>){
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
 
     let c2 = Arc::clone(&counter);
-    let handle2 = thread::spawn(move || {
+    thread::spawn(move || {
         check_pool_size(c2)
     });
-    handles.push(handle2);
 
 
-    let c1 = Arc::clone(&counter);
-    let handle = thread::spawn(move || {
-        handle_one_peer(c1)
-    });
-    handles.push(handle);
+    for _ in 0..100 {
+        let c1 = Arc::clone(&counter);
+        thread::spawn(move || {
+            handle_one_peer(c1)
+        });
+    }
 
 
-    for _ in 0..10 {
+    loop {
         let mut num = counter.lock().unwrap();
         *num += 1;
     }
 
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Result: {}", *counter.lock().unwrap());
 }
