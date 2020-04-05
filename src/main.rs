dea8766e6ecfbcd6b8e935c15c6f80b6648a4926
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-04-05 11:31:31 -0300

    test share adresses to test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::process;
 use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Duration;
-<<<<<<< HEAD
 
 fn handle_one_peer(counter : Arc<Mutex<i64>>){
     for _ in 0..10 {
@@ -29,30 +28,6 @@ fn check_pool_size(counter : Arc<Mutex<i64>>){
 }
 
 
-=======
-
-fn m(counter : Arc<Mutex<i64>>){
-    thread::sleep(Duration::from_millis(1));
-    println!("restando: {}", *counter.lock().unwrap());
-    let mut num = counter.lock().unwrap();
-    *num += -1;
-}
-
-fn c(counter : Arc<Mutex<i64>>){
-    for _ in 0..10 {
-        thread::sleep(Duration::from_millis(1));
-        println!("checkeando: {}", *counter.lock().unwrap());
-
-        if *counter.lock().unwrap() == 0 {
-            println!("chau");
-            process::exit(0);
-        }
-    }
-
-}
-
-
->>>>>>> b7531f6c45f75db7dac6bea0b08e4b0e61573189
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
@@ -60,7 +35,6 @@ fn main() {
 
     let c2 = Arc::clone(&counter);
     let handle2 = thread::spawn(move || {
-<<<<<<< HEAD
         check_pool_size(c2)
     });
     handles.push(handle2);
@@ -73,20 +47,6 @@ fn main() {
     handles.push(handle);
 
 
-=======
-        c(c2)
-    });
-    handles.push(handle2);
-
-    for _ in 0..10 {
-        let c1 = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            m(c1)
-        });
-        handles.push(handle);
-    }
-
->>>>>>> b7531f6c45f75db7dac6bea0b08e4b0e61573189
     for _ in 0..10 {
         let mut num = counter.lock().unwrap();
         *num += 1;
